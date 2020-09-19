2fcd433637d3a9e17520e20d6d07ab8b92e78b47
Erik Vavro <me@madhax.io>
2020-09-19 19:49:15 -0400

    :satellite: :cloud: :scroll: Added useful log messages to /track resource

--- src/bach_rest_api/handler.clj
+++ src/bach_rest_api/handler.clj
@@ -4,18 +4,23 @@
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.util.json-response :refer [json-response]]
+            [ring.middleware.json :only [wrap-json-body]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [clojure.tools.logging :as log]))
 
 (defroutes app-routes
   (GET "/" [] (json-response {:message "Bach REST API"}))
   (POST "/track" {body :body}
-        (try
-          (let [track (-> body slurp ast/parse compile-track)]
-            (json-response track))
-          (catch Exception e
-            (log/error e "Track compilation error")
-            (merge (json-response {:error (:via e)}) {:status 400}))))
+        (let [source (slurp body)]
+          (try
+            (let [track (-> source ast/parse compile-track)]
+              (log/info "[bach-rest-api/track] Valid track compiled. Source Bach data:")
+              (log/info (prn source))
+              (json-response track))
+            (catch Exception e
+              (log/error e "[bach-rest-api/track] Invalid track, compilation FAILED. Source Bach data:")
+              (log/error (prn source))
+              (merge (json-response {:error (:via e)}) {:status 400})))))
   (route/not-found (json-response {:error "Not Found"})))
 
 (def app
